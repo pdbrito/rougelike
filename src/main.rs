523595c2ebf8cd34189d193903de2129935253e3
Pedro De Brito <8843268+pdbrito@users.noreply.github.com>
2022-11-02 00:32:53 +0100

    Give names to monsters and the player

--- src/main.rs
+++ src/main.rs
@@ -73,20 +73,28 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
     gs.ecs.register::<Monster>();
+    gs.ecs.register::<Name>();
 
     let map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(Point::new(player_x, player_y));
 
     let mut rng = rltk::RandomNumberGenerator::new();
-    for room in map.rooms.iter().skip(1) {
+    for (i, room) in map.rooms.iter().skip(1).enumerate() {
         let (x, y) = room.center();
 
         let glyph: rltk::FontCharType;
+        let name: String;
         let roll = rng.roll_dice(1, 2);
         match roll {
-            1 => glyph = rltk::to_cp437('g'),
-            _ => glyph = rltk::to_cp437('o'),
+            1 => {
+                glyph = rltk::to_cp437('g');
+                name = "Goblin".to_string();
+            }
+            _ => {
+                glyph = rltk::to_cp437('o');
+                name = "Orc".to_string()
+            }
         }
 
         gs.ecs
@@ -103,6 +111,9 @@ fn main() -> rltk::BError {
                 dirty: true,
             })
             .with(Monster {})
+            .with(Name {
+                name: format!("{} #{}", &name, i),
+            })
             .build();
     }
 
@@ -124,6 +135,9 @@ fn main() -> rltk::BError {
             visible_tiles: Vec::new(),
             range: 8,
             dirty: true,
+        })
+        .with(Name {
+            name: "Player".to_string(),
         })
         .build();
 
--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,7 +1,7 @@
 use rltk::{console, Point};
 use specs::prelude::*;
 
-use crate::{Monster, Position, Viewshed};
+use crate::{Monster, Name, Viewshed};
 pub struct MonsterAI {}
 
 impl<'a> System<'a> for MonsterAI {
@@ -9,13 +9,14 @@ impl<'a> System<'a> for MonsterAI {
         ReadExpect<'a, Point>,
         ReadStorage<'a, Viewshed>,
         ReadStorage<'a, Monster>,
+        ReadStorage<'a, Name>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (player_pos, viewshed, monster) = data;
-        for (viewshed, _monster) in (&viewshed, &monster).join() {
+        let (player_pos, viewshed, monster, name) = data;
+        for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
             if viewshed.visible_tiles.contains(&*player_pos) {
-                console::log(format!("Monster shouts insults"));
+                console::log(&format!("{} shouts insults", name.name));
             }
         }
     }
