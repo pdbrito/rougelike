b4a6530319ca734467aa1dbba57e3deec4e053b3
Pedro De Brito <8843268+pdbrito@users.noreply.github.com>
2022-11-14 11:57:07 +0100

    Give monsters rudimentary chasing logic

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,22 +1,37 @@
 use rltk::{console, Point};
 use specs::prelude::*;
 
-use crate::{Monster, Name, Viewshed};
+use crate::{Map, Monster, Name, Position, Viewshed};
 pub struct MonsterAI {}
 
 impl<'a> System<'a> for MonsterAI {
     type SystemData = (
+        WriteExpect<'a, Map>,
         ReadExpect<'a, Point>,
-        ReadStorage<'a, Viewshed>,
+        WriteStorage<'a, Viewshed>,
         ReadStorage<'a, Monster>,
         ReadStorage<'a, Name>,
+        WriteStorage<'a, Position>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (player_pos, viewshed, monster, name) = data;
-        for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
+        let (mut map, player_pos, mut viewshed, monster, name, mut position) = data;
+
+        for (mut viewshed, _monster, name, mut pos) in
+            (&mut viewshed, &monster, &name, &mut position).join()
+        {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(&format!("{} shouts insults", name.name));
+                let path = rltk::a_star_search(
+                    map.xy_idx(pos.x, pos.y),
+                    map.xy_idx(player_pos.x, player_pos.y),
+                    &mut *map,
+                );
+                if path.success && path.steps.len() > 1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
